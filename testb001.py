50ee9340c8c528546d3ab635b5cfd61c7854a766
Neil <cloudyang3759@gmail.com>
2021-11-22 19:59:18 +0800

    'a'

--- testb001.py
+++ testb001.py
@@ -113,12 +113,12 @@ def reschedule(event, mtext):
     try:
         flist = mtext[4:].split('/')
         if flist[0] == 'r':
-            sql_cmd="delete from "'"public"'"."+'"'+flist[4]+'"'+" where date =  '"+flist[1]+"', start_slot = "+flist[2]+", end_slot = "+flist[3]+";"
+            sql_cmd="delete from "'"public"'"."+'"'+flist[4]+'"'+" where date =  '"+flist[1]+"' and start_slot = "+flist[2]+" and end_slot = "+flist[3]+";"
             sql_cmd+="insert into "'"public"'"."+'"'+flist[8]+'"'+" values (date =  '"+flist[5]+"', start_slot = "+flist[6]+", end_slot = "+flist[7]+");"
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='reschedule complete！'))
         
         elif flist[0] == 'c':
-            sql_cmd="delete from "'"public"'"."+'"'+flist[4]+'"'+" where date =  '"+flist[1]+"', start_slot = "+flist[2]+", end_slot = "+flist[3]+";"
+            sql_cmd="delete from "'"public"'"."+'"'+flist[4]+'"'+" where date =  '"+flist[1]+"' and start_slot = "+flist[2]+" and end_slot = "+flist[3]+";"
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='delete complete！'))
                 
         db.engine.execute(sql_cmd)
