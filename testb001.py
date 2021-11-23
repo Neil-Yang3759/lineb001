e6e077ec402094f4cdebe7ae7c9dfa78c6352f0d
Neil <cloudyang3759@gmail.com>
2021-11-23 12:00:11 +0800

    'a'

--- testb001.py
+++ testb001.py
@@ -116,7 +116,7 @@ def reschedule(event, mtext):
             # sql_cmd="delete from public."+'"'+flist[4]+'"'+" where date =  '"+flist[1]+"' and start_slot = "+flist[2]+" and end_slot = "+flist[3]+";"
             # sql_cmd2="insert into public."+'"'+flist[8]+'"'+" (date,start_slot,end_slot) values ('"+flist[5]+"', "+flist[6]+", "+flist[7]+");"
             sql_cmd="insert into public."+'"'+flist[8]+'"'+" select date,id,teacher,start_slot,end_slot,tlid from public."+'"'+flist[4]+'"'+" where date =  '"+flist[1]+"' and start_slot = "+flist[2]+" and end_slot = "+flist[3]+";"
-            # sql_cmd+="update public."+'"'+flist[8]+'"'+" set date = '"+flist[5]+"', start_slot = "+flist[6]+" and end_slot = "+flist[7]+";"
+            sql_cmd+="update public."+'"'+flist[8]+'"'+" set date = '"+flist[5]+"', start_slot = "+flist[6]+", end_slot = "+flist[7]+";"
             sql_cmd+="delete from public."+'"'+flist[4]+'"'+" where date =  '"+flist[1]+"' and start_slot = "+flist[2]+" and end_slot = "+flist[3]+";"
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='reschedule complete！'))
             db.engine.execute(sql_cmd)
